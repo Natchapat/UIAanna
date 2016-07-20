cc94df2d34add4815532b8c51864fef3a573d4b7
springsama <spring_maprang@hotmail.com>
2016-07-20 19:54:18 +0700

    Alice3

--- app/src/main/java/com/alice/alpaca/uiaanna/Spktalk.java
+++ app/src/main/java/com/alice/alpaca/uiaanna/Spktalk.java
@@ -2,6 +2,10 @@ package com.alice.alpaca.uiaanna;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.RippleBackground;
 
 /**
  * Created by Alice on 15/7/2559.
@@ -11,6 +15,16 @@ public class Spktalk extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spktalk);
+
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+        ImageView imageView=(ImageView)findViewById(R.id.centerImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rippleBackground.startRippleAnimation();
+            }
+        });
+
 
     }
 }
